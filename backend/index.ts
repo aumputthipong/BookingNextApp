f5c6cb2b7004973b0f06fde1a0382298b3418029
aummnptp <aumputthipong2002@gmail.com>
2024-01-19 21:22:15 +0700

    addExpress+TS

---
+++ backend/index.ts
@@ -0,0 +1,16 @@
+import {Request,Response} from "express"
+const express = require('express')
+const cors = require('cors')
+
+
+const app = express()
+app.use(cors())
+const port = 8080
+
+app.get('/', (req:Request, res: Response) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
