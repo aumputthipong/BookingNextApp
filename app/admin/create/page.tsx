7f60fe7a853a44fac685f133adff211301a1ae59
aummnptp <aumputthipong2002@gmail.com>
2024-02-13 23:07:02 +0700

    Update page.tsx

--- app/admin/create/page.tsx
+++ app/admin/create/page.tsx
@@ -19,6 +19,13 @@ const CreatePage = () => {
           </label>
           <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text"/>
         </div>
+        <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+          <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >
+            จำนวนคนแนะนำต่อห้อง
+          </label>
+          <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text"/>
+        </div>
+
         <button className="btn bg-blue-400">
           สร้าง
         </button>
