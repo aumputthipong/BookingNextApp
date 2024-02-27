4353d3b4aa2a07f89e29c01ea6461cb0111f8cda
aummnptp <aumputthipong2002@gmail.com>
2024-02-27 23:47:03 +0700

    Update page.tsx

--- app/detail/[id]/page.tsx
+++ app/detail/[id]/page.tsx
@@ -19,15 +19,7 @@ const DetailPage = () => {
         <div className="border-solid shadow-xl border-2 w-4/5 rounded-md bg-base-100 mx-1 p-6 ">
           <h1 className="text-2xl font-bold">Creative Room</h1>
           <text className="">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
+            (คำอธิบายห้อง)
           </text>
 
           <h2 className="mb-2 text-lg font-semibold text-gray-900 dark:text-white my-4  ">
@@ -39,13 +31,12 @@ const DetailPage = () => {
           </ul>
         </div>
         {/* column2 */}
-        <div className="border-solid shadow-xl border-2 w-2/5 rounded-md bg-base-100 mx-1 p-6  ">
-          ตรงนี้ควรเป็นform ส่งเพื่อจองห้อง?
-          
-          <form>
-          <input type="string"/>
-          <input type="date" value="2017-06-01" />
-          <button className="btn" name="yoy"/>
+        <div className="border-solid shadow-xl border-2 w-2/5 rounded-md bg-base-100 mx-1 p-6 ">
+          การจอง
+          <form className="col">
+          <input type="string" className="input input-bordered w-24 md:w-auto"/>
+          <input type="date" value="2017-06-01" className="input input-bordered w-24 md:w-auto" />
+          <button className="btn" name="" >จอง</button>
           </form>
 
         </div>
