86f4d8aa3e12d00b2b576163bef0ca7e8ad2a90c
aummnptp <aumputthipong2002@gmail.com>
2024-02-29 21:11:45 +0700

    Update page.tsx

--- app/admin/create/page.tsx
+++ app/admin/create/page.tsx
@@ -11,19 +11,19 @@ const CreatePage = () => {
           <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >
             ชื่อห้อง
           </label>
-          <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text"/>
+          <input className="input input-bordered w-24 md:w-auto" id="grid-first-name" type="text"/>
         </div>
         <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
           <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >
             รายละเอียด
           </label>
-          <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text"/>
+          <input className="input input-bordered w-24 md:w-auto" id="grid-first-name" type="text"/>
         </div>
         <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
           <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >
             จำนวนคนแนะนำต่อห้อง
           </label>
-          <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text"/>
+          <input className="input input-bordered w-24 md:w-auto" id="grid-first-name" type="text"/>
         </div>
 
         <button className="btn bg-blue-400">
